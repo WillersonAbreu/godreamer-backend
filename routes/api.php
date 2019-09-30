b9e5280fb08b295823f650f52ad0b2cd93a65a1b
WillersonAbreu <willersonabreu@hotmail.com>
2019-09-30 08:24:16 -0300

    Término do módulo de amizades

--- routes/api.php
+++ routes/api.php
@@ -1,40 +1,34 @@
 <?php
 
-use Illuminate\Http\Request;
-
 //Rotas Sessão
 Route::put('validarusuario')->middleware('login');
 Route::get('logout')->middleware('logout');
 
 //Rota de Usuários
 Route::prefix('usuarios')->group(function () {
-  Route::get('/', 'UsuarioController@index');
-  Route::post('create', 'UsuarioController@create');
-  Route::put('update', 'UsuarioController@update');
-  Route::delete('delete', 'UsuarioController@delete');
+    Route::get('/', 'UsuarioController@index');
+    Route::post('create', 'UsuarioController@create');
+    Route::put('update', 'UsuarioController@update');
+    Route::delete('delete', 'UsuarioController@delete');
 });
 
 //Rotas de Posts
-Route::prefix('post')->group(function(){
-  Route::get('/', 'PostController@index');
-  Route::post('create','PostController@create');
-  Route::post('update', 'PostController@update');
-  Route::delete('delete','PostController@delete');
-});//->middleware('sessao');
+Route::prefix('post')->group(function () {
+    Route::get('/', 'PostController@index');
+    Route::post('create', 'PostController@create');
+    Route::post('update', 'PostController@update');
+    Route::delete('delete', 'PostController@delete');
+}); //->middleware('sessao');
 
 //Rotas do Feed
-Route::prefix('feed')->group(function(){
-  Route::get('/{id}','FeedController@Index');
-});//->middleware('sessao');
+Route::prefix('feed')->group(function () {
+    Route::get('/{id}', 'FeedController@Index');
+    Route::get('/usuario/{id}', 'FeedController@getPostByUserId');
+}); //->middleware('sessao');
 
 //Rotas amizade
-Route::prefix('amizade')->group(function(){
-  // Route::get('/');
-  Route::post('create', 'AmizadeController@create');
+Route::prefix('amizade')->group(function () {
+    Route::get('/{id}', 'AmizadeController@index');
+    Route::post('create', 'AmizadeController@create');
+    Route::delete('delete', 'AmizadeController@delete');
 });
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
