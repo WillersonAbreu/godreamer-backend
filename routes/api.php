f8b26d3d4556cf0ea7b5b841fabdb23531819862
WillersonAbreu <willersonabreu@hotmail.com>
2019-11-01 21:36:37 -0300

    Inicio da implementaçao do JWT

--- routes/api.php
+++ routes/api.php
@@ -6,29 +6,29 @@ Route::get('logout')->middleware('logout');
 
 //Rota de Usuários
 Route::prefix('usuarios')->group(function () {
-    Route::get('/', 'UsuarioController@index');
-    Route::post('create', 'UsuarioController@create');
-    Route::put('update', 'UsuarioController@update');
-    Route::delete('delete', 'UsuarioController@delete');
+  Route::get('/', 'UsuarioController@index');
+  Route::post('create', 'UsuarioController@create');
+  Route::put('update', 'UsuarioController@update');
+  Route::delete('delete', 'UsuarioController@delete');
 });
 
 //Rotas de Posts
 Route::prefix('post')->group(function () {
-    Route::get('/', 'PostController@index');
-    Route::post('create', 'PostController@create');
-    Route::post('update', 'PostController@update');
-    Route::delete('delete', 'PostController@delete');
+  Route::get('/', 'PostController@index');
+  Route::post('create', 'PostController@create');
+  Route::post('update', 'PostController@update');
+  Route::delete('delete', 'PostController@delete');
 }); //->middleware('sessao');
 
 //Rotas do Feed
 Route::prefix('feed')->group(function () {
-    Route::get('/{id}', 'FeedController@Index');
-    Route::get('/usuario/{id}', 'FeedController@getPostByUserId');
+  Route::get('/{id}', 'FeedController@Index');
+  Route::get('/usuario/{id}', 'FeedController@getPostByUserId');
 }); //->middleware('sessao');
 
 //Rotas amizade
 Route::prefix('amizade')->group(function () {
-    Route::get('/{id}', 'AmizadeController@index');
-    Route::post('create', 'AmizadeController@create');
-    Route::delete('delete', 'AmizadeController@delete');
+  Route::get('/{id}', 'AmizadeController@index');
+  Route::post('create', 'AmizadeController@create');
+  Route::delete('delete', 'AmizadeController@delete');
 });
