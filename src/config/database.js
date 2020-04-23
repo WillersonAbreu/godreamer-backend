3b3c16d29449138990009368392b40f57bab937d
Willerson Abreu <willersonabreu@hotmail.com>
2020-04-23 18:31:56 -0300

    Iniciando o chat

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mariadb',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: '127.0.0.1',//process.env.DB_HOST,
+  username: 'root',//process.env.DB_USERNAME,
+  password: 'mysql',//process.env.DB_PASSWORD,
+  database: 'godreamer',//process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
