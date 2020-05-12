9cedfaaa8835b16b22c143f5c1fcd3661a70ebd5
Willerson Abreu <willersonabreu@hotmail.com>
2020-05-12 19:49:06 -0300

    make it better

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mariadb',
   host: 'us-cdbr-east-06.cleardb.net', //process.env.DB_HOST,
-  username: 'b3e5bd7292539b', //process.env.DB_USERNAME,
-  password: 'caf6ac24', //process.env.DB_PASSWORD,
-  database: 'heroku_af17a7528fb1765', //process.env.DB_NAME,
+  username: 'bf89914ff1dcda', //process.env.DB_USERNAME,
+  password: 'd5eb3e9b', //process.env.DB_PASSWORD,
+  database: 'heroku_7aa8d7b1ad65a5c', //process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
@@ -11,4 +11,4 @@ module.exports = {
   },
 };
 
-//mysql://b3e5bd7292539b:caf6ac24@us-cdbr-east-06.cleardb.net/heroku_af17a7528fb1765?reconnect=true
+//mysql://bf89914ff1dcda:d5eb3e9b@us-cdbr-east-06.cleardb.net/heroku_7aa8d7b1ad65a5c?reconnect=true
