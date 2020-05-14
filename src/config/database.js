38aedfc94c61834a069cb21f17c0d5700234df05
Willerson Abreu <willersonabreu@hotmail.com>
2020-05-14 17:36:00 -0300

    make it better

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mariadb',
   host: 'us-cdbr-east-06.cleardb.net', //process.env.DB_HOST,
-  username: 'b3e5bd7292539b', //process.env.DB_USERNAME,
-  password: 'caf6ac24', //process.env.DB_PASSWORD,
-  database: 'heroku_af17a7528fb1765', //process.env.DB_NAME,
+  username: 'bf89914ff1dcda', //process.env.DB_USERNAME,
+  password: 'd5eb3e9b', //process.env.DB_PASSWORD,
+  database: 'heroku_7aa8d7b1ad65a5c', //process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
