bc6719ee3e37cd3f09242c3037387eebc44dbb94
Willerson Abreu <willersonabreu@hotmail.com>
2020-06-26 06:30:35 -0300

    Pequenos ajustes no layout em geral

---
+++ src/database/migrations/20200625192919-add_about_user_to_user_table.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.addColumn('users', 'about_user', {
+      allowNull: true,
+      type: Sequelize.STRING,
+      defaultValue: null,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:      
+    */
+    return queryInterface.removeColumn('users', 'about_user');
+  },
+};
